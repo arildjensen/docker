95aab11b9ec9152047b4617eb9d8994961ba2526
Arild Jensen <ajensen@counter-attack.com>
2014-06-12 20:04:42 -0400

    Hacker supervisord to work with funambol

--- funambol/run.sh
+++ funambol/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+iname=funambol_i1
+
 docker run \
-  --name funambol_i1 \
+  --name ${iname} \
   -p 0.0.0.0:8080:8080 \
+  -v /root/.ssh:/root/.ssh:ro \
   -d \
   -t \
   -i \
-  funambol \
-  /bin/bash
+  funambol
+
+docker inspect ${iname} | grep IPAddress | awk -F\" '{print $4}'
