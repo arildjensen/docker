086f4600924041b98a8a338eefb24e543a992706
Arild Jensen <ajensen@counter-attack.com>
2014-06-24 00:07:55 -0400

    Added skel templating; modified rundeck since Ubuntu issue has been fixed

---
+++ skel/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+image_name=$(pwd | xargs basename)
+key_dir=/var/docker/${image_name}
+
+if [ ! -d ${key_dir} ]; then
+  mkdir -p  ${key_dir}
+  chmod 700 ${key_dir}
+  cp /root/.ssh/authorized_keys ${key_dir}
+  chmod 600 ${key_dir}/*
+fi 
+
+docker run -p 80:80 \
+           -i \
+           -t \
+           -d \
+           -v ${key_dir}:/root/.ssh:ro \
+           -h ${image_name} \
+           -b ${image_name} \
+           ${image_name}
+
+docker inspect ${iname} | grep IPAddress | awk -F\" '{print $4}'
