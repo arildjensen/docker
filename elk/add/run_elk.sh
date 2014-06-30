785dc0f9ed57d156d129fefb204f7c3415854246
Arild Jensen <ajensen@counter-attack.com>
2014-06-30 14:57:10 -0400

    Functional ELK implementation. Add'l tweaks to logstash conf likely needed.

---
+++ elk/add/run_elk.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ES_HOST=${ES_HOST:-127.0.0.1}
+ES_PORT=${ES_PORT:-9300}
+EMBEDDED="false"
+WORKERS=${ELASTICWORKERS:-1}
+
+if [ "$ES_HOST" = "127.0.0.1" ] ; then
+    EMBEDDED="true"
+fi
+
+cat << EOF > /opt/logstash.conf
+input {
+  syslog {
+    type => syslog
+    port => 514
+  }
+  collectd {typesdb => ["/opt/collectd-types.db"]}
+}
+output {
+  stdout {
+      debug => true
+  }
+
+  elasticsearch {
+      embedded => $EMBEDDED
+      host     => "$ES_HOST"
+      port     => "$ES_PORT"
+      workers  => $WORKERS
+  }
+}
+EOF
+
+
+exec java -jar /opt/logstash.jar agent -f /opt/logstash.conf -- web
